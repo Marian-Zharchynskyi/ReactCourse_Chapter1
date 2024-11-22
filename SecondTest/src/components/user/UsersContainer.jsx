b29f66259f31da26577f27569ef2379688e11c45
Marian-Zharchynskyi <145098603+Marian-Zharchynskyi@users.noreply.github.com>
2024-11-22 11:51:25 +0200

    Updated filtetr and useUsers was deleted

--- SecondTest/src/components/user/UsersContainer.jsx
+++ SecondTest/src/components/user/UsersContainer.jsx
@@ -2,13 +2,12 @@ import React, { useState, useEffect } from 'react'
 import UsersTable from './UsersTable'
 import FetchButton from '../common/FetchButton'
 import FilterBox from '../common/FilterBox'
-import useUsers from '../../hooks/useUsers'
-import { deleteUser } from '../../services/userService'
+import { getUsers, deleteUser } from '../../services/userService'
 import useLoading from '../../hooks/useLoading'
 import Loading from '../common/Loading'
 
 const UsersContainer = () => {
-  const { users, fetchUsers } = useUsers()
+  const [users, setUsers] = useState([])
   const [filter, setFilter] = useState('')
   const [filteredUsers, setFilteredUsers] = useState([])
   const { isLoading, setIsLoading } = useLoading()
@@ -22,10 +21,24 @@ const UsersContainer = () => {
     setFilteredUsers(filtered)
   }, [users, filter])
 
+  const fetchUsers = async () => {
+    setIsLoading(true)
+    try {
+      const data = await getUsers()
+      setUsers(data.data)
+      setFilteredUsers(data.data)
+    } catch (error) {
+      console.error('Error fetching users:', error)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
   const deleteUserById = async (userId) => {
     try {
       setIsLoading(true)
       await deleteUser(userId)
+      setUsers((prev) => prev.filter((user) => user.id !== userId))
       setFilteredUsers((prev) => prev.filter((user) => user.id !== userId))
     } catch (error) {
       console.error('Error deleting user:', error)
@@ -34,22 +47,18 @@ const UsersContainer = () => {
     }
   }
 
-  const handleFetchUsers = async () => {
-    setIsLoading(true)
-    await fetchUsers()
-    setIsLoading(false)
-  }
-
   return (
     <Loading isLoading={isLoading}>
-      {users.length === 0 ? (
-        <FetchButton fetchUsers={handleFetchUsers} />
-      ) : (
-        <>
-          <FilterBox filter={filter} setFilter={setFilter} />
-          <UsersTable users={filteredUsers} onDelete={deleteUserById} />
-        </>
-      )}
+      <div className="UserContainer">
+        {users.length === 0 ? (
+          <FetchButton fetchUsers={fetchUsers} />
+        ) : (
+          <>
+            <FilterBox filter={filter} setFilter={setFilter} />
+            <UsersTable users={filteredUsers} onDelete={deleteUserById} />
+          </>
+        )}
+      </div>
     </Loading>
   )
 }
